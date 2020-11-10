45820217f96f0b07a322f1a0a605ed0ea3a587b3
SantiR38 <santirodriguez38@gmail.com>
2020-11-10 13:01:05 -0300

    App products added.

---
+++ adroit/products/apps.py
@@ -0,0 +1,11 @@
+"""Products App."""
+
+# Django
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    """Products App Config."""
+
+    name = 'adroit.products'
+    verbose_name = 'products'
