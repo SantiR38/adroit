a3a8e193c6bde9e23b1fe2c8589b91b5b5daf98c
SantiR38 <santirodriguez38@gmail.com>
2020-11-11 22:26:16 -0300

    Invoice model implemented and added to model admin.

--- adroit/sales/admin.py
+++ adroit/sales/admin.py
@@ -3,4 +3,28 @@
 # Django
 from django.contrib import admin
 
+# Utilities
+from adroit.sales.models import Invoice
 
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    """Invoice model admin."""
+
+    list_display = ('state_id', 
+                    'payment_method',
+                    'subtotal', 'discount',
+                    'total_discounted', 'total',
+                    'branch_id',)
+
+    search_fields = ('state_id', 
+                     'payment_method',
+                     'total', 'branch_id',
+                     "branch_id__city", "branch_id__state",
+                     "branch_id__country")
+
+    list_filter = ('created', 'modified', 
+                   'payment_method',
+                   'branch_id__code',
+                   "branch_id__city",
+                   "branch_id__state",
+                   "branch_id__country")
--- adroit/sales/models.py
+++ adroit/sales/models.py
@@ -2,3 +2,27 @@
 
 # Django
 from django.db import models
+
+# Utilities
+from adroit.utils.models import AdroitModel
+
+
+class Invoice(AdroitModel):
+    """Invoice model.
+    
+    This model is going to register specifics sales.
+
+    It will be referenced in InvoiceDetail model as
+    a foreign key. 
+    """
+
+    state_id = models.ForeignKey('utils.state', on_delete=models.SET_NULL, null=True)
+    payment_method = models.CharField(max_length=50, default="Cash") # In the forms is going to be an option field
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_discounted = models.DecimalField(max_digits=10, decimal_places=2)
+    # sending_id: one to one(Sending)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    #client_id = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    #seller_id = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    branch_id = models.ForeignKey('branches.Branch', on_delete=models.SET_NULL, null=True)
