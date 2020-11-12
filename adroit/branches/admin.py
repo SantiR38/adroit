884203076bea0f9ee541645e4b92231d5d83179f
SantiR38 <santirodriguez38@gmail.com>
2020-11-11 21:19:43 -0300

    Branch model implemented and added to model admin.

--- adroit/branches/admin.py
+++ adroit/branches/admin.py
@@ -3,4 +3,14 @@
 # Django
 from django.contrib import admin
 
-# Register your models here.
+# Utilities
+from adroit.branches.models import Branch
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    """Branch model admin."""
+
+    list_display = ('code', 'name', 'latitude', 'longitude', 'adress', 'city', 'state', 'country',)
+    search_fields = ('name', 'latitude', 'longitude', 'adress', 'city', 'state', 'country',)
+    list_filter = ('state', 'country',)
+
--- adroit/branches/models.py
+++ adroit/branches/models.py
@@ -3,4 +3,28 @@
 # Django
 from django.db import models
 
+# Utilities
+from adroit.utils.models import AdroitModel
 
+
+class Branch(AdroitModel):
+    """Branch model.
+    
+    With this model, you can specify in the code or in
+    a foreing key where belongs some product.
+    
+    Also, you can create an entire system for a specific branch,
+    with employees and delivery management.
+    """
+
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50, blank=True)
+    latitude = models.CharField(max_length=25)
+    longitude = models.CharField(max_length=25)
+    adress = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Branches'
