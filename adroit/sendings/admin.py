e80b5100557a93c10eaa4c46c8920d432e702ebe
SantiR38 <santirodriguez38@gmail.com>
2020-11-11 23:17:06 -0300

    Sending model implemented and added to model admin.

--- adroit/sendings/admin.py
+++ adroit/sendings/admin.py
@@ -1,6 +1,35 @@
 """Sendings admin."""
 
-#Django
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Utilities
+from adroit.sendings.models import Sending
+
+@admin.register(Sending)
+class SendingAdmin(admin.ModelAdmin):
+    """Sending model admin."""
+
+    list_display = ('user_id', 
+                    'invoice_id',
+                    'latitude',
+                    'longitude',
+                    'branch_id',
+                    'distance',
+                    'going_time',
+                    'arrival_time',
+                    'state_id',)
+
+    search_fields = ('user_id__first_name', 
+                     'user_id__last_name',
+                     'branch_id',
+                     "branch_id__city",
+                     "branch_id__state",
+                     "branch_id__country")
+
+    list_filter = ('created',
+                   'modified',
+                   'branch_id__code',
+                   "branch_id__city",
+                   "branch_id__state",
+                   "branch_id__country")
--- adroit/sendings/models.py
+++ adroit/sendings/models.py
@@ -2,3 +2,26 @@
 
 #Django
 from django.db import models
+
+# Utilities
+from adroit.utils.models import AdroitModel
+
+
+class Sending(AdroitModel):
+    """Sending model.
+    
+    This model is going to register delivery details.
+
+    It will be referenced in Invoice model as
+    a one to one field. 
+    """
+
+    user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    invoice_id = models.ForeignKey('sales.Invoice', on_delete=models.CASCADE)
+    latitude = models.CharField(max_length=25)
+    longitude = models.CharField(max_length=25)
+    branch_id = models.ForeignKey('branches.Branch', on_delete=models.CASCADE)
+    distance = models.CharField(max_length=25)
+    going_time = models.DateTimeField()
+    arrival_time = models.DateTimeField(null=True)
+    state_id = models.ForeignKey('utils.State', on_delete=models.SET_NULL, null=True)
