4212b150db4758a5b4e1a3c142e0fcd6d688ff66
SantiR38 <santirodriguez38@gmail.com>
2020-11-16 08:16:23 -0300

    Created view for listing usernames.

--- adroit/users/urls.py
+++ adroit/users/urls.py
@@ -5,12 +5,13 @@ from django.urls import path
 
 # Utilities
 from . import views
-from .views import ApiEndpoint
+from .views import ApiEndpoint, ListUsers
 
 
 urlpatterns = [
 
-    path('secret', views.secret_page, name='secret'),
-    path('hello', ApiEndpoint.as_view())
+    path('secret/', views.secret_page, name='secret'),
+    path('hello/', ApiEndpoint.as_view()),
+    path('list_users/', ListUsers.as_view()),
 
 ]
--- adroit/users/views.py
+++ adroit/users/views.py
@@ -7,6 +7,15 @@ from oauth2_provider.views.generic import ProtectedResourceView
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+# Rest Framework
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+# Utilities
+from adroit.users.models import User
+
+#---------- Views ----------#
 
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
@@ -16,3 +25,21 @@ class ApiEndpoint(ProtectedResourceView):
 @login_required()
 def secret_page(request, *args, **kwargs):
     return HttpResponse('Secret contents!', status=200)
+
+
+class ListUsers(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    #authentication_classes = [authentication.TokenAuthentication]
+    #permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
